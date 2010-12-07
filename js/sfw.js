4c6db5332f04610a8b80de3f2c33c4091e98c803
Damian Krzeminski <dkrzemin@gmail.com>
2010-12-07 14:18:52 -0500

    jslint fixes

--- js/sfw.js
+++ js/sfw.js
@@ -1,6 +1,8 @@
+/*global localStorage, jQuery */
+
 var sfw = sfw || {};
 
-sfw.yql = (function($) {
+sfw.yql = (function ($) {
 
   function init(spec) {
     var self = {}, memo = {
@@ -16,7 +18,7 @@ sfw.yql = (function($) {
       url += '&env=' + encodeURIComponent(memo.env);
       url += '&format=json&callback=?';
 
-      $.getJSON(url, function(r) {
+      $.getJSON(url, function (r) {
         fn(r.query.results);
       });
     }
@@ -28,9 +30,9 @@ sfw.yql = (function($) {
   return init;
 }(jQuery));
 
-sfw.main = (function($) {
+sfw.main = (function ($) {
   function addTips($container, $template, results, $selector) {
-    $.each(results.tip, function(i, t) {
+    $.each(results.tip, function (i, t) {
       var $p = $template.clone();
       $('.fish', $p).text(t.fish).addClass('tip_' + t.recommendaton);
       $('.recommendation', $p).text(t.recommendaton);
@@ -38,33 +40,33 @@ sfw.main = (function($) {
     });
     $.mobile.pageLoading(true);
     $container.listview('refresh');
-    if($selector) {
+    if ($selector) {
       $selector.addClass('ui-btn-active');
     }
     $.fixedToolbars.show(true);
   }
 
   function onRegionClick(region_id, recommendation, $selector) {
-    var yql = sfw.yql({
-      q : 'select * from seafoodwatch.tip where region_id =' + region_id +
-        ' and recommendation=\"'+ recommendation+'\";'
-    });
+    var $container = $('#tips ul.fish_list'),
+      yql = sfw.yql({
+        q : 'select * from seafoodwatch.tip where region_id =' + region_id +
+          ' and recommendation=\"' + recommendation + '\";'
+      });
     localStorage.current_region_id = region_id;
     localStorage.current_recommendation = recommendation;
-    var $container = $('#tips ul.fish_list');
     $.fixedToolbars.hide(true);
     $container.empty();
     $.mobile.pageLoading();
-    yql.retrieve(function(r) {
+    yql.retrieve(function (r) {
       addTips($container, $('#tip_template li'), r, $selector);
     });
   }
 
   function addRegions($container, $template, results) {
     $container.empty();
-    $.each(results.region, function(i, r) {
+    $.each(results.region, function (i, r) {
       var $p = $template.clone();
-      $('a', $p).text(r.name).attr('id', r.id).click(function() {
+      $('a', $p).text(r.name).attr('id', r.id).click(function () {
         $('#tips h1').text(this.innerText);
         onRegionClick(this.id, localStorage.current_recommendation);
       });
@@ -75,7 +77,7 @@ sfw.main = (function($) {
   }
 
   function addClicks() {
-    $('#tip_selector a').click(function(){
+    $('#tip_selector a').click(function () {
       var recommendation = this.id.slice(7);
       onRegionClick(localStorage.current_region_id, recommendation, $(this));
     });
@@ -88,10 +90,17 @@ sfw.main = (function($) {
       q : 'select * from seafoodwatch.region;'
     });
     $.mobile.pageLoading();
-    yql.retrieve(function(r) {
+    yql.retrieve(function (r) {
       addRegions(spec.container, spec.template, r);
     });
   }
 
   return init;
 }(jQuery));
+
+jQuery(function ($) {
+  sfw.main({
+    container: $('#home ul'),
+    template: $('#region_template li')
+  });
+});
